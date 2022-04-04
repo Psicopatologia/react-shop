f2cd31807bf5237c48892d40dde621f741265e02
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2022-04-03 23:34:02 -0500

    My order show how many products are in cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,12 @@ import menuIcon from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCar from '@icons/icon_shopping_cart.svg';
 import AppContext from '@context/AppContext';
+import MyOrder from '@containers/MyOrder'
 
 const Header = () => {
 	const { state } = useContext(AppContext);
 	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
 	const handleToggle = () => {
 		setToggle(!toggle);
 	}
@@ -44,13 +46,15 @@ const Header = () => {
 					<li className="navbar-email" onClick={ handleToggle }>
 						platzi@example.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-shopping-cart"
+						onClick={() => setToggleOrders(!toggleOrders)}>
 						<img src={ shoppingCar } alt="shopping cart" />
 						{state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
 					</li>
 				</ul>
 			</div>
 			{ toggle && <Menu /> }
+			{ toggleOrders && <MyOrder /> }
 		</nav>
 	);
 }
