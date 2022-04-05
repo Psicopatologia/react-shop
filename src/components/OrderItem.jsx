36e2bdf595274f7c3f6e6212c192e6696c9503c1
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2022-04-04 23:38:43 -0500

    Added option to remove from cart

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '@styles/OrderItem.scss';
-import close from '@icons/icon_close.png'
+import close from '@icons/icon_close.png';
+import AppContext from '@context/AppContext'
 
 const OrderItem = ({product}) => {
+	const {removeFromCart} = useContext(AppContext);
+	const handleRemove = product => {
+		removeFromCart(product);
+	}
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -10,7 +15,7 @@ const OrderItem = ({product}) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={ close } alt="close" />
+			<img src={ close } alt="close" onClick={()=>handleRemove(product)} />
 		</div>
 	);
 }
